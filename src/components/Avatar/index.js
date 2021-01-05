cc186894dba1331b5e95159643a0c2207e8bad7c
hberdu <henriqueberdu@gmail.com>
2021-01-05 10:25:11 -0300

    Adição de Imagem

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+
+const Avatar = () => {
+    const {avatarImage} = useStaticQuery(
+        graphql`
+            query {
+                    avatarImage: file(relativePath: { eq: "profile-photo.jpeg"}) {
+                        childImageSharp {
+                            fluid(maxWidth: 60, maxHeight: 60){
+                                ...GatsbyImageSharpFluid_tracedSVG
+                            }                            
+                        }
+                    }
+            }
+        `
+    )
+
+    return (
+        <Img 
+            fluid={avatarImage.childImageSharp.fluid}
+            className="foo" 
+            style={{ width: "60px" }} 
+        />
+    )
+
+
+}
+
+export default Avatar
